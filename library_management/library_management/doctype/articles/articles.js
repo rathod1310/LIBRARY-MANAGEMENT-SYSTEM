48de213f6ae4b131e4b9cd23d2f272d7f80d7dbc
rathod1310 <rathodom1310@gmail.com>
2025-04-21 19:41:34 +0530

    Fresh commit to fix push issue

---
+++ library_management/library_management/doctype/articles/articles.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Om and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Articles", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ library_management/library_management/doctype/articles/test_articles.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Om and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestArticles(FrappeTestCase):
+	pass
